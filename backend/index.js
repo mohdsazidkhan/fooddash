be89644c8c10f8a374e3013ac486ac6a1cd8f750
SazidKhan <khanmohdsazid@gmail.com>
2024-09-01 14:08:47 +0530

    New Build 1

--- backend/index.js
+++ backend/index.js
@@ -17,9 +17,9 @@ app.use('/api/orders', orderRoutes);
 const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client', 'build')));
+  app.use(express.static(path.join(__dirname, '../frontend', 'build')));
   app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
   })
 }
 
